cb2f3f38eb5060f696716fa6b0c357106eb20d56
AlexandreATavares <aleatavares@hotmail.com>
2020-08-04 20:30:03 -0300

    Add "delete todo" functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,13 @@
 $("li").click(function () {
   $(this).toggleClass("completed");
 });
+
+//click on X to delete todo
+$("span").click(function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
