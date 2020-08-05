e3a1bc3c1e2d08e1527cfe4a560db586107aad92
AlexandreATavares <aleatavares@hotmail.com>
2020-08-04 23:14:25 -0300

    Add button icons and animations

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,6 +17,8 @@ $("input[type='text']").keypress(function (event) {
   if (event.which === 13) {
     var todoText = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $("ul").append(
+      "<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>"
+    );
   }
 });
