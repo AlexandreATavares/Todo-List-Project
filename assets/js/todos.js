d26885d7b777b8a18470394751d8c39ecd086901
AlexandreATavares <aleatavares@hotmail.com>
2020-08-04 21:44:21 -0300

    Add "new todo" functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,22 @@
 //Check off specific todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
   $(this)
     .parent()
     .fadeOut(500, function () {
       $(this).remove();
     });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
